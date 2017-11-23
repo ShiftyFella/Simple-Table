12a0b3a6d4cdb10c40c9848c2b7e4892e5102615
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-11-22 19:03:35 -0500

    Simple Table View Controller

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let dwarves = [ "Sleepy", "Sneezy", "Bashful", "Happy",
+                            "Doc", "Grumpy", "Dopey", "Thorin",
+                            "Dorin", "Nori", "Ori", "Balin",
+                            "Dwalin", "Fili", "Kili", "Oin",
+                            "Gloin", "Bifur", "Bofur", "Bombur"
+    ]
+    
+    let simpleTavleIdentifier = "SimpleTableIdentifier"
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTavleIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: simpleTavleIdentifier)
+        }
+        
+        cell?.textLabel?.text = dwarves[indexPath.row]
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
